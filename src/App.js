21a4ffb90089ea0e65620e308b9cf828183d8f24
Nazar Valchyshyn <nvalja@gmail.com>
2021-02-09 02:20:52 +0200

    connected firebase and some preparetion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
+import firebase from 'firebase';
 
 function App() {
+  const [second, setSecond] = useState(0);
+
+
+  useEffect(() => {
+    let count = 0;
+    let timer = setInterval(() => {
+      console.log('sec');
+      timerStep(count++)
+    }, 1000);
+
+    return() => {
+      clearInterval(timer);
+    }
+  }, []);
+
+  const timerStep = (count) => {
+    setSecond(count);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <>
+      <div className="App">
+        { `${Math.floor(second / 3600)}:${Math.floor(second / 60 % 60)}:${second % 60}` }
+      </div>
+
+      <div className="Form">
+        <h2 className="Form__titile">Login</h2>
+        <form className="Form__inputs">
+          <input type="text" className="Form__input" />
+          <input type="password" className="Form__input" />
+        </form>
+        <input type="button" className="Form__button"/>
+        <a className="Form__link">Don’t have an account yet?<span>Register</span></a>
+      </div>
+
+      <div className="Form">
+        <h2 className="Form__titile">Login</h2>
+        <form className="Form__inputs">
+          <input type="text" className="Form__input" />
+          <input type="password" className="Form__input" />
+        </form>
+        <input type="button" className="Form__button"/>
+        <a className="Form__link">Don’t have an account yet?<span>Register</span></a>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+// useEffect(() => {
+  //   const dataBase = firebase.database();
+
+  //   // console.log(dataBase)
+  // }, []);
+
+  // const handleMail = ({ target }) => {
+  //   setMail(target.value);
+  // };
+
+  // const handlePass = ({ target }) => {
+  //   setPass(target.value);
+  // };
+
+  // const createAccount = () => {
+  //   firebase.auth().createUserWithEmailAndPassword(mail, pass)
+  //     .catch(error => console.log(error));
+
+
+  //   firebase.auth().signInWithEmailAndPassword(mail, pass)
+  //     .catch(error => console.log(error));
+  // }
+  
+
+
+
+
+
+  
+
+  // console.log(mail, pass);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDibq8V-pulg4xQTv4IW4peD8DFCg_zZEI",
+  authDomain: "fire-base-new08.firebaseapp.com",
+  databaseURL: "https://fire-base-new08-default-rtdb.firebaseio.com",
+  projectId: "fire-base-new08",
+  storageBucket: "fire-base-new08.appspot.com",
+  messagingSenderId: "949037149013",
+  appId: "1:949037149013:web:9ec20ba2b1b0a5768cdddc"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
